807718a57efd4af3030f89a5b3555a648d3c0392
isragooprez <isragoo.prez@gmail.com>
2018-01-14 18:38:56 -0500

    Hotfix#01 C11(m1())

--- src/main/java/clases/C11.java
+++ src/main/java/clases/C11.java
@@ -2,8 +2,13 @@ package clases;
 
 public class C11 {
     public static String m1() {
-    
-        return "m1";
+
+        /**
+         * Error hotfix#01
+         */
+        String m1 = "m1";
+
+        return m1;
     }
 
     public static String m2() {
