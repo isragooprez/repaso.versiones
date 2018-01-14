112cb8c1bd35fc70c8478cd23c4fa662b08a69c4
isragooprez <isragoo.prez@gmail.com>
2018-01-14 16:55:22 -0500

    create C11

---
+++ src/main/java/clases/C11.java
@@ -0,0 +1,17 @@
+package clases;
+
+public class C11 {
+    public static String m1() {
+        /**
+         * error return string
+         */
+        String m1 = "m1";
+        return m1;
+    }
+
+    public static String m2() {
+
+        return "m2";
+    }
+
+}
